6aa96020e03b9133ff217911b4d403efde627467
kbaez <karina.baez@dinmax.com>
2018-09-12 17:41:42 -0300

    add new form

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCX7LrRPj5-ZQXzXzgaqE-5ZjjgMaavng4",
+    authDomain: "diary-49d5d.firebaseapp.com",
+    databaseURL: "https://diary-49d5d.firebaseio.com",
+    projectId: "diary-49d5d",
+    storageBucket: "diary-49d5d.appspot.com",
+    messagingSenderId: "104481585185"
+};
+firebase.initializeApp(config);
+
+export const database = firebase.database().ref('/notes')
