845aabf1517327a95a38febfa5c06db3bfe20656
kbaez <kbaez1990@gmail.com>
2018-09-14 00:07:37 -0300

    add handle methods

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
+import {database} from '../firebase';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    //state
+    this.state = {
+      title: '',
+      body: ''
+    };
+
+    //bind
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // handle change
+  handleChange(e){
+    this.setState({
+        [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    const note = {
+      title: this.state.title,
+      body: this.state.body
+    }
+    database.push(note);
+    this.setState({
+      title: '',
+      body: ''
+    })
+  }
+
   render() {
     return (
       <div className="container-fluid">
           <div className="row">
                 <div className="col-sm-6 col-sm-offset-3">
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <div className="form-group">
-                            <input type="text" name="title" className="form-control no-border" placeholder="Title..." required/>
+                            <input onChange={this.handleChange} value={this.state.title} type="text" name="title" className="form-control no-border" placeholder="Title..." required/>
 
                         </div>
                         <div className="form-group">
-                            <textarea type="text" name="body" className="form-control no-border" placeholder="Body..." required/>
+                            <textarea onChange={this.handleChange} value={this.state.body} type="text" name="body" className="form-control no-border" placeholder="Body..." required/>
                         </div>
                         <div className="form-group">
                             <button className="btn btn-primary col-sm-12">Save</button>
