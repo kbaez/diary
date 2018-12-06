2f31945daf7ea59a47ce547b3371dc2dad2e2f1c
kbaez <karina.baez@dinmax.com>
2018-12-06 17:10:55 -0300

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -58,7 +58,13 @@ class App extends Component {
 
           <p>{note.body}</p>
             {note.uid === this.props.user.uid && (
-                <button className="btn btn-danger btn-xs" onClick={()=> this.props.deleteNote(key)}>Delete</button>
+                <div>
+                    <button className="btn btn-danger btn-xs" onClick={()=> this.props.deleteNote(key)}>
+                        Delete
+                    </button>
+                    <button className="btn btn-info btn-xs pull-right"><Link to={`/$key/edit`}>Update</Link>
+                    </button>
+                </div>
             )}
         </NoteCard>
       )
--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ import Header from './routes/Header';
 import LoadingComponent from './components/LoadingComponent';
 import AuthenticatedComponent from './components/AuthenticatedComponent';
 import NoteDetail from './components/NoteDetail';
+import NoteEdit from './components/NoteEdit';
 
 //create redux store -> reducers -> 'actions'
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
@@ -31,6 +32,7 @@ ReactDOM.render(
                         <Header/>
                         <Route path="/" component={App} exact={true}/>
                         <Route path="/:id" component={NoteDetail} exact={true} />
+                        <Route path="/:id/edit" component={NoteEdit} exact={true} />
                     </AuthenticatedComponent>
                 </Switch>
             </div>
